1ca3f60257f3a7df019cd3b426b4559a3f38abfc
Peter Heldens <peter.heldens@live.nl>
2020-11-13 18:08:17 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,3 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let meter = speedometer.create(DiameterOrRadius.Diameter, 28, Units.Inches)
+meter.init(Runstate.Start)
